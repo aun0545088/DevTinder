a47aea5ff0313670f92306efeb2fa4e35998ea8f
Aun Mohammad <aun0545088@gmail.com>
2025-03-16 14:46:32 +0530

    Done:Authentication JWT cookies and set mongoose methods

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const app = express()
 const User = require("./models/user")
 const { validateSignUpData } = require("./utils/validation")
 const bcrypt = require("bcrypt")
+const cookieParser = require("cookie-parser")
+const jwt = require("jsonwebtoken")
+const { userAuth } = require("./middlewares/auth")
 
 app.use(express.json())
 
@@ -43,13 +46,28 @@ app.post('/login', async (req, res) => {
         if (!user) {
             throw new Error("Invalid credentials.")
         }
-        const isPasswordValid = bcrypt.compare(password, user.password)
+        const isPasswordValid = user.validatePassword(password)
 
         if (isPasswordValid) {
+
+            // create a JWT token
+            const token = await user.getJWT()
+
+            // Add the token to cookie and send back the respond to the user
+            res.cookie("token", token)
             res.send("login successfully!")
         } else {
             throw new Error("Invalid credentials.")
         }
+    } catch (err) {
+        res.status(400).send(`Error saving the user: ${err.message}`)
+    }
+})
+
+app.get("/profile", userAuth, async (req, res) => {
+    try {
+        const user = req.user
+        res.send(user)
     } catch (err) {
         res.status(400).send(`Error saving the user: ${err.message}`)
     }
---
+++ src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+
+const userAuth = async (req, res, next) => {
+    try {
+        //Read the token from the req cookies
+        if (!token) {
+            throw new Error("Token is not valid!")
+        }
+        const { token } = req.cookies
+        //Validate the token
+        const decodedObj = await jwt.verify(token, "secretKey")
+
+        const { _id } = decodedObj
+        //Find the username
+        const user = await User.findById(_id)
+
+        if (!user) {
+            throw new Error("User not found.")
+        }
+        req.user = user
+        next()
+    } catch {
+        res.status(400).send(`ERROR: ${res.message}`)
+    }
+}
+
+module.exports = {
+    userAuth,
+}
