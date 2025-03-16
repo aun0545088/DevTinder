450031a7e3e086091f64932f78a0c00668c0e17d
Aun Mohammad <aun0545088@gmail.com>
2025-03-16 17:30:59 +0530

    Add: express router

--- src/app.js
+++ src/app.js
@@ -1,77 +1,15 @@
 const express = require("express")
 const connectDB = require("./config/database")
 const app = express()
-const User = require("./models/user")
-const { validateSignUpData } = require("./utils/validation")
-const bcrypt = require("bcrypt")
 const cookieParser = require("cookie-parser")
-const jwt = require("jsonwebtoken")
-const { userAuth } = require("./middlewares/auth")
 
 app.use(express.json())
+app.use(cookieParser())
 
-app.post('/signup', async (req, res) => {
-    console.log(req.body)
-    //Validation of data
+const authRouter = require("./routes/auth")
+const profileRouter = require("./routes/profile")
+const requestRouter = require("./routes/requests")
 
-    try {
-        validateSignUpData(req)
-
-        const { firstName, lastName, emailId, password } = req.body
-
-        //Encryption of password
-        const passwordHash = bcrypt.hash(password, 10)
-
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password: passwordHash
-        })
-
-        await user.save()
-        res.send("User added successfully!")
-    }
-    catch (err) {
-        res.status(400).send(`Error saving the user: ${err.message}`)
-    }
-})
-
-app.post('/login', async (req, res) => {
-    try {
-        const { emailId, password } = req.body
-
-        const user = await User.findOne({ emailId: emailId })
-
-        if (!user) {
-            throw new Error("Invalid credentials.")
-        }
-        const isPasswordValid = user.validatePassword(password)
-
-        if (isPasswordValid) {
-
-            // create a JWT token
-            const token = await user.getJWT()
-
-            // Add the token to cookie and send back the respond to the user
-            res.cookie("token", token)
-            res.send("login successfully!")
-        } else {
-            throw new Error("Invalid credentials.")
-        }
-    } catch (err) {
-        res.status(400).send(`Error saving the user: ${err.message}`)
-    }
-})
-
-app.get("/profile", userAuth, async (req, res) => {
-    try {
-        const user = req.user
-        res.send(user)
-    } catch (err) {
-        res.status(400).send(`Error saving the user: ${err.message}`)
-    }
-})
 
 connectDB()
     .then(() => {
---
+++ src/routes/auth.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
+const validateSignUpData = require("../utils/validation")
+const User = require("../models/user")
+
+const authRouter = express.Router()
+
+authRouter.post('/signup', async (req, res) => {
+    console.log(req.body)
+    //Validation of data
+
+    try {
+        validateSignUpData(req)
+
+        const { firstName, lastName, emailId, password } = req.body
+
+        //Encryption of password
+        const passwordHash = bcrypt.hash(password, 10)
+
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash
+        })
+
+        await user.save()
+        res.send("User added successfully!")
+    }
+    catch (err) {
+        res.status(400).send(`Error saving the user: ${err.message}`)
+    }
+})
+
+authRouter.post('/login', async (req, res) => {
+    try {
+        const { emailId, password } = req.body
+
+        const user = await User.findOne({ emailId: emailId })
+
+        if (!user) {
+            throw new Error("Invalid credentials.")
+        }
+        const isPasswordValid = user.validatePassword(password)
+
+        if (isPasswordValid) {
+
+            // create a JWT token
+            const token = await user.getJWT()
+
+            // Add the token to cookie and send back the respond to the user
+            res.cookie("token", token)
+            res.send("login successfully!")
+        } else {
+            throw new Error("Invalid credentials.")
+        }
+    } catch (err) {
+        res.status(400).send(`Error saving the user: ${err.message}`)
+    }
+})
+
+module.exports = authRouter
