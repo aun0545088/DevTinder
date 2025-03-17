7979e388302f2c85f41f8b491d22d2368957f381
Aun Mohammad <aun0545088@gmail.com>
2025-03-17 15:32:28 +0530

    adding the logout and edit profile features

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const authRouter = require("./routes/auth")
 const profileRouter = require("./routes/profile")
 const requestRouter = require("./routes/requests")
 
+app.use("/", authRouter)
+app.use("/", profileRouter)
+app.use("/", requestRouter)
+
 
 connectDB()
     .then(() => {
